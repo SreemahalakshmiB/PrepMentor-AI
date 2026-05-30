50053154ab16caad882af3a773962379642e500d
SreemahalakshmiB <sreemahalakshmi539@gmail.com>
2026-05-30 13:44:34 +0530

    Updated frontend with mood based schedule

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       setLoading(true);
 
       const response = await axios.post(
-        "http://127.0.0.1:5000/generate-plan",
+        "https://prepmentor-backend.onrender.com/generate-plan",
         formData
       );
 
